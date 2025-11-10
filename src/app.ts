66b603679ffbcdb077de9f24aa73a5dd841ee144
Glueeed <swiderskikuba12@gmail.com>
2025-11-10 15:28:28 +0100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,72 @@
+import express, { Request, Response } from 'express';
+import path from "path";
+import cors from 'cors';
+import https from 'https';
+
+
+//Uncomment when httpsMode is enabled
+// import {options} from "./config/ssl";
+
+import {corsEnabled, httpsMode, PORT, domain} from "./configs/settings";
+
+// Import Routes
+
+
+
+const app = express();
+const port : number = PORT;
+
+
+//Uncomment when httpsMode is enabled
+
+// const ssl = options
+
+
+//Middlewares
+app.use(express.json());
+if (corsEnabled) {
+    const corsOptions = {
+        origin: domain,
+        credentials: true,
+        optionsSuccessStatus: 200,
+    };
+    app.use(cors());
+}
+
+
+
+// Frontend handling
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req: Request, res: Response)  => {
+    res.sendFile(path.join(__dirname, 'public', '/views' ,'test.html'));
+})
+
+// API endpoints
+
+
+
+
+
+
+
+
+if (httpsMode) {
+
+    // https.createServer(ssl, app).listen(port, "0.0.0.0", () => {
+    //
+    //     console.log(`App running at ${domain}:${port}`);
+    //
+    // });
+
+} else {
+    app.listen(port, () => {
+        console.log(`App running at ${domain}:${port}`);
+
+    });
+
+}
+
+
+
---
+++ src/configs/ssl.ts
@@ -0,0 +1,8 @@
+// import fs from 'fs';
+// import path from 'path';
+//
+//  export const options : object = {
+//     key: fs.readFileSync("/your/path/key"),
+//     cert: fs.readFileSync("/your/path/cert"),
+//     ca: fs.readFileSync("/your/path/chain"),
+// };
