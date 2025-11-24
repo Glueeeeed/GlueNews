2115e971451ddd1625ff9e1980fb81f002e6e1dd
Glueeed <swiderskikuba12@gmail.com>
2025-11-24 17:09:37 +0100

    docs: Dodano readme

---
+++ readme.md
@@ -0,0 +1,70 @@
+# GlueNews 
+
+**GlueNews** – aplikacja konkursowa stworzona na HackHeroes 2025  
+Najlepsze miejsce do sprawdzania wiarygodności informacji i ćwiczenia umiejętności argumentacji w trybie rzeczywistej debaty!
+
+![GlueNews banner](./src/public/gluenews.png)
+
+## Założenia projektu
+
+GlueNews to narzędzie stworzone jako odpowiedź na co raz wiekszą ilość fałszywych informacji w Internecie. Celem projektu było stworzenie narzędzia które pomoże rozróżnić fałszywe informacje od tych prawdziwych wykorzystując do tego sztuczną inteligencje która staje się naszą przyszłością.
+
+
+## Funkcje aplikacji
+
+- **Weryfikacja informacji** – użytkownicy mogą zgłaszać artykuły lub wiadomości do weryfikacji przez AI.
+- **Debaty na żywo** – użytkownicy mogą uczestniczyć w debatach na żywo (real-time), gdzie widownia może głosować na najbardziej przekonujące argumenty.
+- **System punktacji** – użytkownicy zdobywają punkty za aktywność i trafność swoich wypowiedzi oraz sensowne argumenty.
+- **Ranking użytkowników** – tablica liderów pokazująca najbardziej aktywnych i skutecznych uczestników debat.
+- **Interfejs użytkownika** – responsywny i intuicyjny interfejs użytkownika dostosowany do różnych urządzeń.
+- **System logowania oraz bezpieczeństwo** – bezpieczne logowanie z dodatkową warstwą ochrony danych logowania podczas przesyłu. Zaimplementowano szyfrowanie danych na czas przesyłu za pomoca AES-256-GCM z jednorazowym kluczem sesyjnym ustalanym przy pomocy [ECDH](https://en.wikipedia.org/wiki/Elliptic-curve_Diffie%E2%80%93Hellman).
+
+## Technologie użyte w projekcie
+
+- **Frontend**: HTML, Tailwind CSS, JavaScript, Socket.IO Client (real-time)
+
+- **Backend**: Node.js, Express, TypeScript, Socket.io, MySQL
+
+- **Kryptografia i bezpieczeństwo**: argon2, noble-ciphers
+
+- **API AI**: Gemini API
+
+- **Inne narzędzia**: Github, WebStorm, Postman
+
+## Wymagania
+
+- Node.js ≥ 20
+- MySQL 8.0+
+- Klucz API do Gemini
+
+## Instalacja i uruchomienie
+
+1. Sklonuj repozytorium:  
+   ```
+   git clone https://github.com/Glueeeeed/GlueNews.git
+   ```
+2. Zainstaluj zależności:  
+   ```
+   npm install
+   ```
+3. Skonfiguruj zmienne środowiskowe w pliku `.env`.
+
+4. Użyj pliku `database.sql` do stworzenia struktury bazy danych. Plik znajduje się w katalogu głównym repozytorium.
+
+5. Uruchom bazę danych MySQL i zaimportuj strukturę bazy danych z pliku `database.sql`.
+
+6. Uruchom aplikację:  
+   ```
+   npm run start
+   ```
+
+## Uwaga 
+Modele AI mogą generować nieprawdziwe lub nieodpowiednie treści. Użytkownicy powinni korzystać z aplikacji odpowiedzialnie i weryfikować informacje z innych źródeł.
+   
+## Licencja
+Projekt jest dostępny na licencji MIT. Szczegóły w pliku LICENSE.
+
+
+
+
+
--- src/services/analyseService.ts
+++ src/services/analyseService.ts
@@ -21,32 +21,32 @@ export function extractJsonFromMarkdown(text: any): any | null {
 }
 
 export async function getAnalysisResult(input: string, key: any , model: string): Promise<any> {
-        const { GoogleGenAI } = await import("@google/genai");
-        const ai = new GoogleGenAI({ apiKey: key});
-        const response = await ai.models.generateContent({
-            model: model,
-            contents: 'Jesteś detektorem fake newsów dla nastolatków. Analizuj krok po kroku:\n' +
-                '\n' +
-                `Temat lub link: ${input}\n` +
-                '\n' +
-                '1. Wczytaj treść z linku (jeśli 404 → verdict: "Błąd linku") lub zapoznaj sie z treścią tematu.,\n' +
-                '2. Wyodrębnij 3 kluczowe twierdzenia.,\n' +
-                '3. Dla każdego: sprawdź via web search, podaj cytację w tekście.,\n' +
-                '4. Oceń wiarygodność źródła (WHOIS, Media Bias Chart). Szukaj zrodel NIE STARSZYCH niz 6 miesiecy,\n' +
-                '5. Daj score 0-100 i verdict.,\n' +
-                '6. Analizuj czy zrodla działają (nie są martwe).,\n' +
-                '\n' +
-                'Odpowiedz TYLKO  W JSON wedlug wzoru:\n' +
-                '\n' +
-                '{\n' +
-                '  "truth_score": 0-100,\n' +
-                '  "verdict": "Bzdura / Częściowa prawda / Prawda / Błąd linku",\n' +
-                '  "reasons": ["Twierdzenie 1: fałsz, bo [cytat z Reuters]", ...],\n' +
-                '  "sources": ["https://...", ...],\n' +
-                '}',
-        });
+    const { GoogleGenAI } = await import("@google/genai");
+    const ai = new GoogleGenAI({ apiKey: key});
+    const response = await ai.models.generateContent({
+        model: model,
+        contents: 'Jesteś detektorem fake newsów dla nastolatków. Analizuj krok po kroku:\n' +
+            '\n' +
+            `Temat lub link: ${input}\n UWAGA: Sprawdz czy jest merytorycznie (TYLKO W PRZYPADKU TEMATU) poprawny, jezeli nie ma sensu np. to jedno słowo np. "pies" ignoruj i zwroc że to nie ma sensu` +
+            '\n' +
+            '1. Wczytaj treść z linku (jeśli 404 → verdict: "Błąd linku") lub zapoznaj sie z treścią tematu.,\n' +
+            '2. Wyodrębnij 3 kluczowe twierdzenia.,\n' +
+            '3. Dla każdego: sprawdź via web search, podaj cytację w tekście.,\n' +
+            '4. Oceń wiarygodność źródła (WHOIS, Media Bias Chart). Szukaj zrodel NIE STARSZYCH niz 6 miesiecy,\n' +
+            '5. Daj score 0-100 i verdict.,\n' +
+            '6. Analizuj czy zrodla działają (nie są martwe).,\n' +
+            '\n' +
+            'Odpowiedz TYLKO  W JSON wedlug wzoru:\n' +
+            '\n' +
+            '{\n' +
+            '  "truth_score": 0-100,\n' +
+            '  "verdict": "Bzdura / Częściowa prawda / Prawda / Błąd linku",\n' +
+            '  "reasons": ["Twierdzenie 1: fałsz, bo [cytat z Reuters]", ...],\n' +
+            '  "sources": ["https://...", ...],\n' +
+            '}',
+    });
 
-        return extractJsonFromMarkdown(response.text);
+    return extractJsonFromMarkdown(response.text);
 
 }
 
